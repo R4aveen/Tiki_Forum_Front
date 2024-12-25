3d615f57127edb4c1ae0c5260a3592a4a692eee9
r4veeen <bas.rojasl@duocuc.cl>
2024-12-25 13:05:44 -0300

    arreglo de banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -17,7 +17,7 @@ function Home() {
 	}, [dispatch]);
 
 
-	
+
 	return (
 		<PageWrapper isProtectedRoute={true} title='Home'>
 			<Subheader>
@@ -26,29 +26,41 @@ function Home() {
 				</SubheaderLeft>
 					
 			</Subheader>
-			<Container className='flex h-full w-full flex-col items-center'>
-				{loading && <p>Loading...</p>}
-				{error && <p>Error: {error}</p>}
-				<div className='foros-list grid grid-cols-1 gap-8 w-full max-w-6xl h-90'>
-					{foros.map((foro) => (
-						<Card key={foro.id} className='mb-6 shadow-xl transition-transform transform hover:scale-105'>
-							<CardHeader className='bg-gradient-to-r from-blue-600 to-purple-600 p-6 text-white'>
-								<h1 className='text-2xl font-bold'>{foro.nombre}</h1>
-							</CardHeader>
-							<CardBody className='p-6'>
-								<div className='mb-6 max-h-80 w-full overflow-hidden rounded-lg'>
-									<img
-										className='h-full w-full object-cover'
-										src={foro.imagen}
-										alt={foro.nombre}
-									/>
-								</div>
-								<p className='text-gray-800'>{foro.descripcion}</p>
-							</CardBody>
+				<Container className='flex h-full w-full flex-col items-center'>
+					{loading && <p>Loading...</p>}
+					{error && <p>Error: {error}</p>}
+					<div className='w-full max-w-9xl mb-8'>
+						<Card className='relative'>
+							<img
+								className='w-full h-74 object-cover rounded-lg'
+								src='https://via.placeholder.com/1200x300/FFFF00/000000?text=Bienvenidos'
+								alt='Bienvenidos'
+							/>
+							<div className='absolute inset-0 flex items-center justify-center'>
+								<h1 className='text-4xl font-bold text-white'>Bienvenidos</h1>
+							</div>
 						</Card>
-					))}
-				</div>
-			</Container>
+					</div>
+					<div className='foros-list grid grid-cols-1 gap-8 w-full max-w-6xl mt-8 mb-16'>
+						{foros.map((foro) => (
+							<Card key={foro.id} className='mb-6 shadow-xl transition-transform transform hover:scale-105'>
+								<CardHeader className='bg-gradient-to-r from-blue-600 to-purple-600 p-6 text-white'>
+									<h1 className='text-2xl font-bold'>{foro.nombre}</h1>
+								</CardHeader>
+								<CardBody className='p-6'>
+									<div className='mb-6 max-h-80 w-full overflow-hidden rounded-lg'>
+										<img
+											className='h-full w-full object-cover'
+											src={foro.imagen}
+											alt={foro.nombre}
+										/>
+									</div>
+									<p className='text-gray-800'>{foro.descripcion}</p>
+								</CardBody>
+							</Card>
+						))}
+					</div>
+				</Container>
 		</PageWrapper>
 	);
 }
